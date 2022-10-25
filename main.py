a080baf41146a2f46104e231d67bd77fa6ed10b3
SecondThundeR <awayfromgalaxy@gmail.com>
2022-10-25 20:48:01 +0300

    style: slightly reformat main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import asyncio
 import aiohttp
 
-
 SUCCESS_COLOR = '\033[92m'
 FAIL_COLOR = '\033[91m'
 NO_COLOR = '\033[0m'
 PURPLE_COLOR = '\033[95m'
 CYAN_TEXT = '\033[96m'
 BOLD_TEXT = '\033[1m'
-
 
 
 async def fetch(session, url):
@@ -18,13 +16,16 @@ async def fetch(session, url):
 
 async def fetch_all(urls, loop):
     async with aiohttp.ClientSession(loop=loop) as session:
-        results = await asyncio.gather(*[fetch(session, url) for url in urls], return_exceptions=True)
+        results = await asyncio.gather(
+            *[fetch(session, url) for url in urls],
+            return_exceptions=True)
         return results
 
 
 def get_formulae_urls():
     import sys
-    return [f'https://formulae.brew.sh/api/formula/{line.strip()}.json' for line in sys.stdin.readlines()]
+    return [f'https://formulae.brew.sh/api/formula/{line.strip()}.json'
+            for line in sys.stdin.readlines()]
 
 
 def check_for_ventura_support(formulae_data):
@@ -32,13 +33,18 @@ def check_for_ventura_support(formulae_data):
 
     for formula in formulae_data:
         name = formula['name']
-        ventura_bottle = formula.get('bottle', {}).get('stable', {}).get('files', {}).get('arm64_ventura')
+        ventura_bottle = formula.get('bottle', {})\
+            .get('stable', {})\
+            .get('files', {})\
+            .get('arm64_ventura')
 
-        if ventura_bottle:
-            print(f'{SUCCESS_COLOR}Formulae "{name}" has a bottle for Ventura{NO_COLOR}')
-            supported_formulae.append(name)
-        else:
-            print(f'{FAIL_COLOR}Formulae "{name}" does not have a bottle for Ventura{NO_COLOR}')
+        if not ventura_bottle:
+            print(f'{FAIL_COLOR}Formulae "{name}" does '
+                  f'not have a bottle for Ventura{NO_COLOR}')
+            continue
+
+        print(f'{SUCCESS_COLOR}Formulae "{name}" has a bottle for Ventura{NO_COLOR}')
+        supported_formulae.append(name)
 
     return f'brew reinstall {" ".join(supported_formulae)}'
 
@@ -47,8 +53,10 @@ def main():
     loop = asyncio.get_event_loop_policy().get_event_loop()
     formulae_urls = get_formulae_urls()
     formulae_data = loop.run_until_complete(fetch_all(formulae_urls, loop))
+
     print("Bottle Ventura support check results:")
     reinstall_command = check_for_ventura_support(formulae_data)
+
     print(f'{PURPLE_COLOR}{"=" * 20}{NO_COLOR}'
           f'\n{BOLD_TEXT}Run the following command to reinstall '
           f'the formulae that have a bottle for Ventura:{NO_COLOR}\n'
